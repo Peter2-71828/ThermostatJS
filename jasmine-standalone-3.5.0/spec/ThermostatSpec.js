839259dfcef895d9e3b0fa1c1e12c91f10a6b1b9
Peter <pc.dean@icloud.com>
2020-02-19 13:54:23 +0000

    remove repeaded creation of new Thermostat into beforeEach block

--- jasmine-standalone-3.5.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.5.0/spec/ThermostatSpec.js
@@ -1,47 +1,43 @@
 describe("Thermostat", function() {
 
+  beforeEach( function() {
+    thermostat = new Thermostat()
+  });
+
   it("Should have a base value of 20", function() {
-    thermostat = new Thermostat();
     expect(thermostat.current_temp()).toEqual(20)
   });
 
   it("up should increase temp by 1", function() {
-    thermostat = new Thermostat();
     thermostat.up();
     expect(thermostat.current_temp()).toEqual(21);
   })
 
   it("up should decrease temp by 1", function() {
-    thermostat = new Thermostat();
     thermostat.down();
     expect(thermostat.current_temp()).toEqual(19);
   })
 
   it("should not decrease below a temperature of 10", function() {
-    thermostat = new Thermostat();
     thermostat._temperature = 10;
     thermostat.down();
     expect(thermostat.current_temp()).toEqual(10);
   })
 
   it("should toggle powersaving mode", function() {
-    thermostat = new Thermostat();
     thermostat.TogglePowerSave();
     expect(thermostat._powerSaver).toEqual(false);
   })
 
   it("by default max temp is 25", function() {
-    thermostat = new Thermostat();
     thermostat._temperature = 25;
     thermostat.up();
     expect(thermostat.current_temp()).toEqual(25);
   })
 
   it("if not inn powerSaver max temp is 32", function() {
-    thermostat = new Thermostat();
-    thermostat._temperature = 31;
+    thermostat._temperature = 32;
     thermostat.TogglePowerSave();
-    thermostat.up()
     thermostat.up()
     expect(thermostat.current_temp()).toEqual(32);
   })
